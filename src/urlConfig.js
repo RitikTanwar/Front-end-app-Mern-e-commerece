762944442fe6aa25c14d2f8daf4bc1d63a302b87
ritik.tanwar00@gmail.com <ritik.tanwar00@gmail.com>
2021-06-17 02:44:43 +0530

    pls

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,4 +1,7 @@
-export const api = "http://localhost:3000/api";
+// export const api = "http://localhost:3000/api";
+const baseURL = "https://flipkart-backened-rest-server.herokuapp.com";
+
+export const api = `${baseURL}/api`;
 export const generatePublicURL = (filename) => {
   return `http://localhost:3000/public/${filename}`;
 };
